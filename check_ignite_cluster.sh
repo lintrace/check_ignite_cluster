b4421ad4253980bf7a32e52f3aa5c0f3504ac993
Alex <>
2024-04-22 22:39:55 +0300

    small_fixes

--- check_ignite_cluster.sh
+++ check_ignite_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-############################################################
+###############################################################################
 #
 #  Проверка серверов кластера для развертывания Ignite 
 #
@@ -16,7 +16,26 @@
 #  8. Пользователь ignite (uid, gid, группы, sudo, chage)
 #  9. Проверка на корректность юнита ignite-server
 #
-###########################################################
+###############################################################################
+
+# Variables
+# ports to check
+declare -ri ssh_port=22   # remote hosts SSH port
+
+declare -ra ports_to_check=(
+    11211   # JDBC port (and for cli tools like control.sh)
+    10800   # thin client port
+    47100   # local communication port
+    47500   # local discovery port
+    49128   # java JMX port
+    49129   # java RMI port
+    8080    # REST (Web API)
+    8443    # Secure REST (https Web API)
+)
+
+declare -r ignite_user="ignite"  # user on remote hosts for ignite
+
+###############################################################################
 
 args=$#         # Количество переданных аргументов скрипту
 server_list=$1  # Переданный файл со списокм адресов для проверки
@@ -29,7 +48,7 @@ print_line () {
     echo "${line}"
 }
 
-# ------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 if [ ${args} -ne 1 ]; then 
     echo ""
@@ -44,28 +63,29 @@ if [ ${args} -ne 1 ]; then
     exit 1
 fi
 
-# ------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 for server in $(cat "${server_list}"); do 
-    # Проверка доступности по порту 22 (ssh)
-    if ! nc -z "${server}" 22; then 
+    # Проверка доступности по порту SSH
+    if ! nc -z "${server}" ${ssh_port}; then
         echo -e "\n> Узел  ${server} недоступен!  (!!!)"
         print_line '=' 80
         continue
     fi
 
     echo -e "\n> Узел:  ${server}"
+    ssh_conn_str=" -q ${server} -p ${ssh_port}"
+
     # Порт 22 уже проверен, а остальные проверяем только если запущен ignite
-    ignite_runned="$( ssh -q "${server}" "ps aux | grep [i]gnite.sh | wc -l")" 
+    declare -i ignite_runned="$( ssh "${ssh_conn_str}" "ps aux | grep [i]gnite.sh | wc -l")"
     if [ ${ignite_runned} -eq 0 ]; then
-        echo -ne "\n# Проверка портов: 22-OK, порты Ignite не проверялись, так как он не запущен!"
+        echo -ne "\n# Проверка портов: 22-OK. Порты Ignite не проверялись, так как процесс не запущен!"
     else
         # Определяем версию Ignite для стандартных путей
-        ignite_version=$(ssh -q ${server} "sudo -u ignite find /opt/ignite/server/libs/ -type f -name ignite-core*.jar 2>/dev/null | grep -Po '[0-9.]+'")
+        ignite_version=$(ssh "${ssh_conn_str}" "sudo -u ignite find /opt/ignite/server/libs/ -type f -name ignite-core*.jar 2>/dev/null | grep -Po '[0-9.]+'")
         echo -ne "\n# Обнаружен запущенный Ignite версии ${ignite_version}\n# Проверка портов: 22-OK"
-        for port in 11211 10800 1098 1099 8080 8443; do
-            nc -z ${server} ${port}
-            if [ $? -eq 0 ] ; then
+        for port in ${ports_to_check[@]}; do
+            if nc -z ${server} ${port} ; then
                 echo -n ", ${port}-OK"
             else
                 echo -n ", ${port}-FAILED(!)"
@@ -74,25 +94,27 @@ for server in $(cat "${server_list}"); do
      fi
      echo ""
 
+     continue
+
      # Проверка ОЗУ и файла подкачки
-     ram_total=$(ssh -q ${server} "free -h | grep Mem | awk '{print \$2}'")
-     swap_total=$(ssh -q ${server} "free -h | grep Swap | awk '{print \$2}'")
+     ram_total=$(ssh "${ssh_conn_str}" "free -h | grep Mem | awk '{print \$2}'")
+     swap_total=$(ssh "${ssh_conn_str}" "free -h | grep Swap | awk '{print \$2}'")
      echo -e "\n# Память (total) - ОЗУ: ${ram_total}, \tSwap: ${swap_total}"
 
      # Информация о процессоре
-     cpu_core=$(ssh -q ${server} "lscpu | egrep '^CPU\\(' | awk '{print \$2}' ")
-     cpu_model=$(ssh -q ${server} "lscpu | grep '^Model name' | awk '{\$1=\$2=\"\"; print \$0}' ")
-     cpu_hyper=$(ssh -q ${server} "lscpu | grep '^Hypervisor' | awk '{print \$3}' ")
-     cpu_virt=$(ssh -q ${server} "lscpu | grep '^Virtualization' | awk '{print \$3}' ")
+     cpu_core=$(ssh "${ssh_conn_str}" "lscpu | egrep '^CPU\\(' | awk '{print \$2}' ")
+     cpu_model=$(ssh "${ssh_conn_str}" "lscpu | grep '^Model name' | awk '{\$1=\$2=\"\"; print \$0}' ")
+     cpu_hyper=$(ssh "${ssh_conn_str}" "lscpu | grep '^Hypervisor' | awk '{print \$3}' ")
+     cpu_virt=$(ssh "${ssh_conn_str}" "lscpu | grep '^Virtualization' | awk '{print \$3}' ")
 
      echo -e "\n# Ядер - ${cpu_core}, процессор: ${cpu_model}, виртуализация ${cpu_hyper}, ${cpu_virt}"
 
      # Проверка точек монтирования и владельцев
      echo -e "\n\n# Точки монтирования /opt/*"
-     ssh -q ${server} "lsblk -o MOUNTPOINT,SIZE | grep '/opt/' | sort -u"
+     ssh "${ssh_conn_str}" "lsblk -o MOUNTPOINT,SIZE | grep '/opt/' | sort -u"
 
-     for mount_path in $(ssh -q ${server} "lsblk -o MOUNTPOINT | grep '/opt/' | sort -u"); do
-         root_files=$(ssh -q ${server} "ls -l ${mount_path} | grep root")
+     for mount_path in $(ssh "${ssh_conn_str}" "lsblk -o MOUNTPOINT | grep '/opt/' | sort -u"); do
+         root_files=$(ssh "${ssh_conn_str}" "ls -l ${mount_path} | grep root")
          if [ -n "${root_files}" ]; then
              echo -e "\nВ точке монтирования ${mount_path} обнаружены файлы с владельцем  root!"
              echo ${root_files}
@@ -100,24 +122,24 @@ for server in $(cat "${server_list}"); do
      done
 
      # Проверка версии java
-     java_version=$(ssh -q ${server} "java --version | head -n1")
+     java_version=$(ssh "${ssh_conn_str}" "java --version | head -n1")
      echo -e "\n\n# Используемая java: ${java_version}"
 
      # Проверка версии OS Linux
-     pretty_name_os=$(ssh -q ${server} 'grep PRETTY_NAME /etc/os-release | cut -d\" -f2')
-     kernel=$(ssh -q ${server} "uname -r")
+     pretty_name_os=$(ssh "${ssh_conn_str}" 'grep PRETTY_NAME /etc/os-release | cut -d\" -f2')
+     kernel=$(ssh "${ssh_conn_str}" "uname -r")
      echo -e "\n# Операционная система: ${pretty_name_os}, ядро: ${kernel}"
 
      # Проверка пользователя techstack
-     techstack_uid=$(ssh -q ${server} "grep techstack /etc/passwd | cut -d: -f3")
-     techstack_gid=$(ssh -q ${server} "grep techstack /etc/passwd | cut -d: -f4")
-     techstack_groups=$(ssh -q ${server} "groups techstack | cut -d: -f2")
-     if [ $(ssh -q ${server} "chage -l techstack | grep never | wc -l") -eq 3 ]; then
+     techstack_uid=$(ssh "${ssh_conn_str}" "grep techstack /etc/passwd | cut -d: -f3")
+     techstack_gid=$(ssh "${ssh_conn_str}" "grep techstack /etc/passwd | cut -d: -f4")
+     techstack_groups=$(ssh "${ssh_conn_str}" "groups techstack | cut -d: -f2")
+     if [ $(ssh "${ssh_conn_str}" "chage -l techstack | grep never | wc -l") -eq 3 ]; then
         techstack_chage="OK"
      else
         techstack_chage="Необходимо сменить пароль!"
      fi 
-     if [ $(ssh -q ${server} "sudo -l | grep '\(ignite\) NOPASSWD: ALL' | wc -l") -eq 1 ]; then
+     if [ $(ssh "${ssh_conn_str}" "sudo -l | grep '\(ignite\) NOPASSWD: ALL' | wc -l") -eq 1 ]; then
         techstack_sudo="OK"
      else
         techstack_sudo="FAIL!"
@@ -125,15 +147,15 @@ for server in $(cat "${server_list}"); do
      echo -e  "\n# Пользователь techstack: chage - ${techstack_chage}, sudo - ${techstack_sudo}, UID=${techstack_uid}, GID=${techstack_gid}, входит в группы:${techstack_groups}"
 
      # Проверка пользователя ignite
-     ignite_uid=$(ssh -q ${server} "grep ignite /etc/passwd | cut -d: -f3")
-     ignite_gid=$(ssh -q ${server} "grep ignite /etc/passwd | cut -d: -f4")
-     ignite_groups=$(ssh -q ${server} "groups ignite | cut -d: -f2")
-     if [ $(ssh -q ${server} "sudo -u ignite chage -l ignite | grep never | wc -l") -eq 3 ]; then
+     ignite_uid=$(ssh "${ssh_conn_str}" "grep ignite /etc/passwd | cut -d: -f3")
+     ignite_gid=$(ssh "${ssh_conn_str}" "grep ignite /etc/passwd | cut -d: -f4")
+     ignite_groups=$(ssh "${ssh_conn_str}" "groups ignite | cut -d: -f2")
+     if [ $(ssh "${ssh_conn_str}" "sudo -u ignite chage -l ignite | grep never | wc -l") -eq 3 ]; then
         ignite_chage="OK"
      else
         ignite_chage="Необходимо сменить пароль!"
      fi 
-     if [ $(ssh -q ${server} "sudo -u ignite sudo -l | grep '\(ALL\)' | wc -l") -ge 30 ]; then
+     if [ $(ssh "${ssh_conn_str}" "sudo -u ignite sudo -l | grep '\(ALL\)' | wc -l") -ge 30 ]; then
         ignite_sudo="OK"
      else
         ignite_sudo="FAIL! Необходимо проверить на соответствие новому чек-листу"
@@ -141,7 +163,7 @@ for server in $(cat "${server_list}"); do
      echo -e  "\n# Пользователь ignite: chage - ${ignite_chage}, sudo - ${ignite_sudo}, UID=${ignite_uid}, GID=${ignite_gid}, входит в группы:${ignite_groups}"
 
      # Проверка юнита ignite-server ( что pid ищется по правильному пути )
-     if [ $(ssh -q ${server} "grep 'PIDFile=/opt/ignite/logs/ignite-server.pid' /etc/systemd/system/ignite-server.service | wc -l" ) -ne 1 ]; then
+     if [ $(ssh "${ssh_conn_str}" "grep 'PIDFile=/opt/ignite/logs/ignite-server.pid' /etc/systemd/system/ignite-server.service | wc -l" ) -ne 1 ]; then
         echo -e "\n# Внимание! Некорректный юнит ignite-server.service! Не использовать запуск череез systemctl без правки юнита!"
      fi
 
