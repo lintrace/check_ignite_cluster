f408ebeda5ab3c50aba5ff69d757bd81038fd7b0
Alex <>
2024-04-27 21:03:05 +0300

    Added some useful check

--- check_ignite_cluster.sh
+++ check_ignite_cluster.sh
@@ -79,6 +79,7 @@ user_check () {
     local ssh_conn=$2
     local sudo_check_string=$3
     local sudo_check_matches=$4
+
     local grep_passwd_out="$(ssh ${ssh_conn} "grep ${user} /etc/passwd")"
     if [ -z "${grep_passwd_out}" ]; then
         echo -e "\n# ВНИМАНИЕ! Пользователя ${user} не существует!"
@@ -87,17 +88,24 @@ user_check () {
     local user_uid="$(echo ${grep_passwd_out} | cut -d: -f3)"
     local user_gid="$(echo ${grep_passwd_out} | cut -d: -f4)"
     local user_groups="$(ssh ${ssh_conn_str} "groups ${user}" | cut -d: -f2)"
-    if [ $(ssh ${ssh_conn} "chage -l ${user}" | grep -c "never") -ge 3 ]; then
+
+    local sudo_str=""
+    if [ "${user}" != "${maintenance_user}" ]; then
+        sudo_str="sudo -u ${user}"
+    fi
+
+    if [ $(ssh ${ssh_conn} "${sudo_str} chage -l ${user}" | grep -c "never") -ge 3 ]; then
         local user_chage="OK"
     else
         local user_chage="Необходимо сменить пароль и/или отключить срок действия пароля!"
     fi
-    if [ $(ssh ${ssh_conn} "sudo -l" | grep -c "${sudo_check_string}") -ge "${sudo_check_matches}" ]; then
+    if [ $(ssh ${ssh_conn} "${sudo_str} sudo -l" | grep -c "${sudo_check_string}") -ge "${sudo_check_matches}" ]; then
         local user_sudo="OK"
     else
         local user_sudo="FAIL! (необходимо проверить вывод sudo -l на соответствие чек-листу)"
     fi
     echo -e  "\n# Пользователь ${user}: chage - ${user_chage}, sudo - ${user_sudo}, UID=${user_uid}, GID=${user_gid}, входит в группы:${user_groups}"
+
     return 0
 }
 
@@ -106,15 +114,15 @@ user_check () {
 args=$#         # Количество переданных аргументов скрипту
 server_list=$1  # Переданный файл со списокм адресов для проверки
 
-if [ ${args} -ne 1 ]; then 
+if [ ${args} -ne 1 ]; then
     echo ""
-    print_line '!' 75 
+    print_line '!' 75
     echo "!  ВНИМАНИЕ!  В  качестве параметра скрипту необходимо  передать файл с   !"
     echo "!  перечнем ip-адресов или FQDN серверных узлов по одному узлу в строке,  !"
     echo "!  либо узлы могут быть перечислены в одну строку через пробел.           !"
     echo "!  Пример:                                                                !"
     echo "!      check_the_cluster.sh /<path_to_your_file>/ip.txt                   !"
-    print_line '!' 75 
+    print_line '!' 75
     echo ""
     exit 1
 fi
@@ -127,7 +135,7 @@ else
     sudo_str=""
 fi
 
-for server in $(cat "${server_list}"); do 
+for server in $(cat "${server_list}"); do
     # Проверка доступности по порту SSH
     if ! nc -z ${server} ${ssh_port}; then
         echo -en "\n> Узел ${server} недоступен по порту ${ssh_port} (ssh)"
@@ -142,7 +150,7 @@ for server in $(cat "${server_list}"); do
 
     echo -e "\n> Узел:  ${server}"
     ssh_conn_str=" -q ${server} -p ${ssh_port} -l ${maintenance_user}"
-    
+
     ps_cmd_out="$( ssh ${ssh_conn_str} "ps aux | grep '[i]gnite.sh.*/libs/\*'")"
     if [ $? -eq 255 ]; then
         echo "Не удалось подключиться через ssh по порту ${ssh_port}! Проверьте логин [maintenance_user] и ключи ssh!"
@@ -158,7 +166,15 @@ for server in $(cat "${server_list}"); do
         run_ignite_libs_path="$(echo "${ps_cmd_out}" | rev | grep -Po "\*/sbil/[^(\s|:)]+" | rev | cut -d* -f1)"
         run_ignite_version=$(ssh ${ssh_conn_str} "${sudo_str} find ${run_ignite_libs_path} -type f -name ignite-core-*jar 2>/dev/null" | grep -Po '[\d]+\.[\d]+\.[\d]+')
         run_ignite_user="$(echo ${ps_cmd_out} | awk '{print $1}')"
-        echo -ne "\n# Запущен Ignite версии ${run_ignite_version} под пользователем ${run_ignite_user}\n# Проверка портов: 22-OK"
+
+        # Определяем персистентность по конфигурации СУ
+        run_ignite_conf_path="$( echo "${ps_cmd_out}" | grep -Po "[^' ']+.xml")"
+        run_ignite_type="in-memory"
+        if [ "$(ssh ${ssh_conn_str} "${sudo_str} grep -Pic 'persistenceEnabled.+true' "${run_ignite_conf_path}"  ")" -ge "1" ]; then
+            run_ignite_type="PERSISTENCE - требуется активация/деактивация!"
+        fi
+
+        echo -ne "\n# Запущен Ignite версии ${run_ignite_version} под пользователем ${run_ignite_user}, тип: ${run_ignite_type}\n# Проверка портов: 22-OK"
         for port in "${ports_to_check[@]}"; do
             if nc -z ${server} "${port}" ; then
                 echo -n ", ${port}-OK"
@@ -170,7 +186,7 @@ for server in $(cat "${server_list}"); do
      echo ""
 
      # Проверка ОЗУ и файла подкачки
-     free_cmd_out="$(ssh ${ssh_conn_str} "free -h")" 
+     free_cmd_out="$(ssh ${ssh_conn_str} "free -h")"
      ram_total="$(echo "${free_cmd_out}" | grep Mem | awk '{print $2}')"
      swap_total="$(echo "${free_cmd_out}" | grep Swap | awk '{print $2}')"
      echo -e "\n# Память (total) - ОЗУ: ${ram_total}, \tSwap: ${swap_total}"
@@ -193,7 +209,7 @@ for server in $(cat "${server_list}"); do
      if [ -n "${fs_mount_points_for_ignite}" ]; then
          lsblk_cmd_out="$(ssh ${ssh_conn_str} "lsblk -o MOUNTPOINT,SIZE" | grep "${fs_mount_points_for_ignite}" | sort -u)"
          echo -en "\n# Точки монтирования в ${fs_mount_points_for_ignite}*"
-         if [ -z "${lsblk_cmd_out}" ]; then 
+         if [ -z "${lsblk_cmd_out}" ]; then
              echo " отсутствуют!"
          else
              echo -e "\n${lsblk_cmd_out}"
